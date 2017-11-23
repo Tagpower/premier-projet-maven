c227d7b0d42c02674747e41fdcada5dc9553170a
Clément Bauchet <clement.bauchet44@gmail.com>
2017-11-23 15:18:21 +0100

    properties & profils

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,7 @@
 package dev;
 
+import java.util.ResourceBundle;
+
 import com.github.lalyos.jfiglet.FigletFont;
 
 /**
@@ -8,8 +10,13 @@ import com.github.lalyos.jfiglet.FigletFont;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println(FigletFont.convertOneLine("Hello World!"));
+    public static void main( String[] args ) {
+    	String titre = ResourceBundle.getBundle("application").getString("titre");
+    	String asciiArt = FigletFont.convertOneLine(titre);
+    	System.out.println(asciiArt);    
+    	
+    	//affichage de l'environnement
+    	String environnement = ResourceBundle.getBundle("application").getString("environnement");
+    	System.out.println("Environnement : " + environnement);
     }
 }
